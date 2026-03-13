167045c84960b930468a07083b234b8f003a8feb
Berry_Hunter <tank-20@list.ru>
2026-03-13 13:32:05 +0300

    hw ready

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 import os
+import sys
+
 from dotenv import load_dotenv
 from pathlib import Path
 from celery.schedules import crontab
@@ -189,3 +191,11 @@ CACHES = {
         'LOCATION': 'redis://127.0.0.1:6379/1',
     }
 }
+
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'test.db.sqlite3',
+        }
+    }
